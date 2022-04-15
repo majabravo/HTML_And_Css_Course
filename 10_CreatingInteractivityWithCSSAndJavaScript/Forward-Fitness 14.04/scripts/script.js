a841a08767eb1576fa0df7c8bd53735e7abbcbb5
majabravo <mtripunoska@gmail.com>
2022-04-15 21:02:28 +0200

    added changes

--- 10_CreatingInteractivityWithCSSAndJavaScript/Forward-Fitness 14.04/scripts/script.js
+++ 10_CreatingInteractivityWithCSSAndJavaScript/Forward-Fitness 14.04/scripts/script.js
@@ -3,34 +3,28 @@
     File Name: script.js
     Data: 14.04.2022
 */
-var videoSource = document.getElementById("vid-src");
-var video = document.getElementById("example");
-var descriptionSource = document.getElementById("despsrc");
-// Hamburger menu funtion 
-function hamburger() {
-    var menu = document.getElementById("menu-links");
-    var logo = document.getElementById("ffc-logo");
-    if (menu.style.display === "block" && logo.style.display === "none") {
-        menu.style.display = "none";
-        logo.style.display = "block";
-    } else {
-        menu.style.display = "block";
-        logo.style.display = "none" ;
-         }
-    }
+//Global variables
+var video=document.getElementById("example");
+var videoSource=document.getElementById("vid-src");
+var descriptionSource=document.getElementById("despsrc");
+video.style.width = "500px";
+video.style.height = "300px";
 
-  // Function to display plank video
-function plank() {
-    console.log(videoSource);
-    videoSource.src = "./media/plank.mp4";
-    // descriptionSource.src = "media/plankdescriptions.vtt"; 
+//Function to display the burpees example video
+ function burpees() {
+videoSource.src="./media/burpees.mp4";
+video.style.display="block";
+video.load();
+ }
+ //Function to display plank video
+ function plank() {
+    videoSource.src="./media/plank.mp4";
+    video.style.display="block";
+    video.load();
+     }
+      // Function to display climbers video
+function climbers() {
+    videoSource.src = "./media/climbers.mp4";
     video.style.display = "block";
     video.load();
-}
-    //Function to display a promo code
-function discount () {
-    var promo = document.getElementById("special")
- promo.firstChild.nodeValue = "Promo Code: D25START";
- promo.style.color = "#ff0000";
- promo.style.fontSize = "2em";
  }
