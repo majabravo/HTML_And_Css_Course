f78a0ab42a3f46df04c3c9a508b25527b6c98ecf
majabravo <mtripunoska@gmail.com>
2022-05-05 21:01:18 +0200

    added changes

---
+++ Bootstrap/Fitness/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Maja Bravo
+    File Name: script.js 
+    Date: 21.04.2022
+*/
+
+// JQuery for hero image to consume the header window space
+$(document).ready(function () {
+    $(".hero").height($(window).height())
+});
---
+++ JavaScripts/Esentionals/ex2.2/main.js
@@ -0,0 +1,8 @@
+let str= "Hello";
+let name= "Maja";
+let age= "36";
+let codeJavaScript=true;
+
+console.log(str + " my name is " + name+ " I am " + age + " years old, " + "and I can code Javascript."+codeJavaScript);
+
+
