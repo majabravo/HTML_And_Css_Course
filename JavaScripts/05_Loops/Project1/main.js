eb381c894709774407b7df7a090d6195ea762936
majabravo <mtripunoska@gmail.com>
2022-05-20 21:00:57 +0200

    added project1 of JS loop

---
+++ JavaScripts/05_Loops/Project1/main.js
@@ -0,0 +1,15 @@
+let tableArray = [];
+let numberOfMultiplication = 7;
+
+for (let i = 0; i < ; i++) {numberOfMultiplication
+  let tempArray = [];
+  for (let j = 0; j < numberOfMultiplication; j++) {
+    if(i * j === 0) 
+    tempArray.push(i * j){
+continue;}
+
+  }
+  tempArray.push(tempArray);
+  
+}
+console.table(tableArray);
